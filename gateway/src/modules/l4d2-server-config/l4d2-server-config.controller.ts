8dea27c34349c8300dab90e17b48c21202f8b4d7
thanhlq <lequanhlol@gmail.com>
2025-09-12 14:45:01 +0700

    update cache

--- gateway/src/modules/l4d2-server-config/l4d2-server-config.controller.ts
+++ gateway/src/modules/l4d2-server-config/l4d2-server-config.controller.ts
@@ -1,32 +1,16 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, UseInterceptors } from '@nestjs/common';
 import { L4d2ServerConfigService } from './l4d2-server-config.service';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import type { Cache } from 'cache-manager';
+import { ApiCacheInterceptor } from 'src/common/interceptor/api-cache.interceptor';
 
 @Controller('l4d2-server-config')
+@UseInterceptors(ApiCacheInterceptor)
 export class L4d2ServerConfigController {
   constructor(
     private readonly l4d2ServerConfigService: L4d2ServerConfigService,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   @Get('getMotd')
   async getMotd(): Promise<string> {
-    const cacheKey = '/l4d2-server-config/getMotd';
-
-    // Kiểm tra cache
-    const cachedData = await this.cacheManager.get<string>(cacheKey);
-    if (cachedData) {
-      console.log('Returning cached data for getMotd');
-      return cachedData;
-    }
-
-    // Nếu chưa có cache thì gọi service
-    const data = await this.l4d2ServerConfigService.getMotd();
-
-    // Lưu vào cache với TTL = 5 phút
-    await this.cacheManager.set(cacheKey, data);
-
-    return data;
+    return this.l4d2ServerConfigService.getMotd();
   }
 }
