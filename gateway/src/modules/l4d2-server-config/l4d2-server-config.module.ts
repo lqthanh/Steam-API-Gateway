1cc733abeba2404d946da33f27607aed756bfd55
Le Quang Thanh <thanhlq@iceteasoftware.com>
2025-09-23 16:35:16 +0700

    update

--- gateway/src/modules/l4d2-server-config/l4d2-server-config.module.ts
+++ gateway/src/modules/l4d2-server-config/l4d2-server-config.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { L4d2ServerConfigController } from './l4d2-server-config.controller';
 import { L4d2ServerConfigService } from './l4d2-server-config.service';
+import { SteamCommunityService } from '../steam-community/steam-community.service';
 
 @Module({
   controllers: [L4d2ServerConfigController],
-  providers: [L4d2ServerConfigService],
+  providers: [L4d2ServerConfigService, SteamCommunityService],
 })
 export class L4d2ServerConfigModule {}
